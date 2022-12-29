0395008271666185070bae8ad8789a805faf6384
Benjamin Brown <bpbrown@gmail.com>
2022-12-28 22:35:08 -0800

    Adds eigenvector plotting.

--- roberts_flow1.py
+++ roberts_flow1.py
@@ -10,6 +10,9 @@ Usage:
 Options:
     --N=<N>           Resolution in x, y, z [default: 16]
     --lambda=<λ>      Resistivity [default: 1/8]
+    --dense           Solve densely for all eigenvalues (slow)
+
+    --verbose         Show plots on screen
 """
 
 import logging
@@ -68,8 +71,54 @@ problem.add_equation("div(A) + τ_φ = 0")
 problem.add_equation("integ(φ) = 0")
 solver = problem.build_solver()
 
-#solver.solve_dense(solver.subproblems[0], rebuild_matrices=True)
-solver.solve_sparse(solver.subproblems[0], N=20, target=0.25, rebuild_matrices=True)
+if args['--dense']:
+    solver.solve_dense(solver.subproblems[0], rebuild_matrices=True)
+else:
+    solver.solve_sparse(solver.subproblems[0], N=10, target=0.5, rebuild_matrices=True)
 i_evals = np.argsort(solver.eigenvalues.real)
+if args['--dense']:
+    i_evals = i_evals[0:len(i_evals)//2]
+# else:
+#     i_evals = i_evals[-7:]
 evals = solver.eigenvalues[i_evals]
+ks = np.arange(10)+1
 print(evals)
+
+import matplotlib.pyplot as plt
+
+n_modes = 4 #8
+i_modes = slice(-n_modes*2,None,2)
+fig, ax = plt.subplots(nrows=(n_modes+1), ncols=2, figsize=[12,6])
+ax[0,0].plot(ks, evals.real, linestyle='none', marker='o')
+ax[0,1].plot(ks, evals.imag, linestyle='none', marker='o')
+
+# reduce number of plotted eigenmodes
+i_evals = i_evals[i_modes]
+evals = solver.eigenvalues[i_evals]
+ax[0,0].plot(ks[i_modes], evals.real, linestyle='none', marker='o')
+ax[0,1].plot(ks[i_modes], evals.imag, linestyle='none', marker='o')
+
+ax[0,0].set_ylabel(r'$\omega_R$')
+ax[0,1].set_ylabel(r'$\omega_I$')
+curl = lambda A: de.Curl(A)
+B_op = curl(A)
+for n, idx in enumerate(i_evals, start=1):
+    solver.set_state(idx, solver.subsystems[0])
+    # B = B_op.evaluate()
+    # B.change_scales(1)
+    i_max = np.unravel_index(np.argmax(np.abs(A['g'][0]), axis=None), A['g'][0].shape)
+    norm = A['g'][0][i_max]
+    print(i_max, norm)
+    Ag = (A['g']/norm).real
+    #ax[1].plot(x[:,0,0], Bg[0][:,0,0]/np.max(np.abs(Bg)), label=f"n={n}")
+    print(np.min(Ag[0].imag), np.max(Ag[0].imag))
+    #ax[n].pcolormesh(y[0,:,0], z[0,0,:], Ag[0][0,:,:])
+    ax[n,0].pcolormesh(x[:,0,0], z[0,0,:], Ag[0][:,0,:], cmap='RdYlBu_r')
+    ax[n,1].pcolormesh(y[0,:,0], z[0,0,:], Ag[0][0,:,:], cmap='RdYlBu_r')
+    ax[n,0].set_ylabel('$\omega_R$={:.2f}'.format(evals[n-1].real))
+    ax[n,1].set_ylabel('$\omega_I$={:.2f}'.format(evals[n-1].imag))
+
+#fig.tight_layout()
+if args['--verbose']:
+    plt.show()
+fig.savefig('evals_roberts_flow1.png', dpi=300)
